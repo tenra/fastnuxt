4503b5871f80d62d39763458fc7507ceb0f0e149
tenra <tetenchi@yahoo.co.jp>
2021-09-24 17:59:35 +0900

    router_top

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
-async def index():
-    return {"message": "Hello World"}
+async def top():
+    return "Hello, FastVue"
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios';
+import Vue from 'vue';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+import router from './router';
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:3000/';  // the backend
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
+//createApp(App).mount('#app')
